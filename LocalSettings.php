bf75114c70f909fe53d30a1d6f41da73f21a180d
Andrea Brioschi <andreabrioschi@MacBook-Air-di-Andrea.local>
2021-04-16 12:01:29 +0200

    edit sitename

--- LocalSettings.php
+++ LocalSettings.php
@@ -20,7 +20,7 @@ if (!defined('MEDIAWIKI')) {
 # $wgDisableOutputCompression = true;
 
 $wgSitename = "Wiki Rca Assistenza";
-$wgMetaNamespace = "Wiki Rca Assistenza";
+$wgMetaNamespace = "Wiki_Rca_assistenza";
 
 ## The URL base path to the directory containing the wiki;
 ## defaults for all runtime URL paths are based off of this.
